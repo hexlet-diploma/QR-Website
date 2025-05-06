0a7d611426b9a158d996a48171ad76aa62788e8d
Elena <lekolupaeva1201@gmail.com>
2025-05-06 18:07:19 +0400

    add features block

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -1,9 +1,46 @@
 import React from "react";
-import { Typography, Box, Stack, useTheme } from "@mui/material";
+import { Typography, Box, Stack, useTheme, Grid } from "@mui/material";
+import FeatureBlock from "../FeatureBlock";
 
 const Features = () => {
   const theme = useTheme();
-
+  const features = [
+    {
+      type: "generate",
+      description:
+        "Generate QR Codes from URLs on the fly! Our extension automatically creates a QR code of the currently open page so you can share it in one click.",
+      isAndMore: false,
+    },
+    {
+      type: "link",
+      description:
+        "Generate QR Codes from any link you want! Just paste the link into the extension and it will create a QR code for you.",
+      isAndMore: false,
+    },
+    {
+      type: "copy",
+      description:
+        "Copy the generated QR Code to your clipboard with one click! No need to download it first.",
+      isAndMore: false,
+    },
+    {
+      type: "download",
+      description:
+        "Download the generated QR Code in PNG format with one click! No need to open it in a new tab.",
+      isAndMore: false,
+    },
+    {
+      type: "color",
+      description:
+        "Choose the color of your QR Code! You can select any color you want for your QR Code.",
+      isAndMore: false,
+    },
+    {
+      type: "andMore",
+      description: "And more!",
+      isAndMore: true,
+    },
+  ];
   return (
     <Stack spacing={2}>
       <Stack direction="row" alignItems="center" gap="20px">
@@ -30,7 +67,17 @@ const Features = () => {
           </Typography>
         </Box>
       </Stack>
-      {/* Add more content here */}
+      <Grid container alignSelf="center" sx={{ maxWidth: "80%" }} rowSpacing={2.5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        {features.map((feature, index) => (
+          <Grid item xs={12} sm={6} md={6} key={index}>
+            <FeatureBlock
+              type={feature.type}
+              description={feature.description}
+              isAndMore={feature.isAndMore}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </Stack>
   );
 };
